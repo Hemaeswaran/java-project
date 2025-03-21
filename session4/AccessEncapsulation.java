cf1fd3c15d046fb0b76bcde1d333cc9be6ea2023
Hemaeswaran <hema02042005@gmail.com>
2025-03-21 15:20:14 +0530

    session-04

---
+++ session4/AccessEncapsulation.java
@@ -0,0 +1,15 @@
+package session4;
+
+public class AccessEncapsulation {
+	
+	public static void main(String[] args) {
+		Encapsulation ee = new Encapsulation();
+		ee.setName("Hema");
+		ee.setLocation("Coimbatore");
+		
+		System.out.println(ee.getName());
+		System.out.println(ee.getLocation());
+		
+	}
+
+}
